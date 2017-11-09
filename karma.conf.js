95e3e2a930f455f1d0bbb82c082221b7e782090a
Morten Olsen <morten@olsen.io>
2017-11-09 01:17:15 +0100

    test: Added karma and saucelabs support

--- karma.conf.js
+++ karma.conf.js
@@ -1,10 +1,39 @@
 /** @format */
 
+const customLaunchers = {
+  sl_chrome: {
+    base: 'SauceLabs',
+    browserName: 'chrome',
+    version: '61',
+  },
+  sl_firefox: {
+    base: 'SauceLabs',
+    browserName: 'firefox',
+    version: '52',
+  },
+  /*sl_ios_safari: {
+    base: 'SauceLabs',
+    browserName: 'iphone',
+    version: '11.0'
+  },*/
+  /*sl_ios_android: {
+    base: 'SauceLabs',
+    browserName: 'Android',
+    platformVersion: '7.1'
+  },*/
+  sl_edge: {
+    base: 'SauceLabs',
+    browserName: 'microsoftedge',
+    platform: 'Windows 10',
+  },
+};
+
 module.exports = function(config) {
   config.set({
     frameworks: ['mocha', 'karma-typescript'],
     singleRun: true,
     files: [
+      'node_modules/babel-polyfill/dist/polyfill.js',
       { pattern: 'src/**/*.ts' }, // *.tsx for React Jsx
       { pattern: 'test/**/*.ts' },
     ],
@@ -14,7 +43,12 @@ module.exports = function(config) {
     karmaTypescriptConfig: {
       tsconfig: './tsconfig.json',
     },
-    reporters: ['progress', 'karma-typescript'],
-    browsers: ['ChromeHeadless'],
+    sauceLabs: {
+      testName: 'Clash of Robots',
+    },
+    reporters: ['progress', 'saucelabs', 'karma-typescript'],
+    //browsers: ['ChromeHeadless'],
+    customLaunchers: customLaunchers,
+    browsers: Object.keys(customLaunchers),
   });
 };
